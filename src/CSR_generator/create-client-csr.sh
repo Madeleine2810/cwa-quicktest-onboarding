f43df35c5b51e52afee5fc898f410831da6a07f4
Madeleine2810 <98094045+Madeleine2810@users.noreply.github.com>
2022-01-20 13:18:51 +0100

    Update create-client-csr.sh

--- src/CSR_generator/create-client-csr.sh
+++ src/CSR_generator/create-client-csr.sh
@@ -3,25 +3,25 @@
 if ! command -v openssl > /dev/null 2>&1; then
     echo "openssl must be installed"
     exit 1
-fi
+fi Teststelle Weingarten
 
 if test ${#1} -lt 3; then
     echo "Please provide organisation name at least 3 charachters long"
     exit 1
-fi
+fi Teststelle Weingarten 
 
-NAME_PROD=${1}-prod
-NAME_WRU=${1}-wru
+Teststelle Weingarten_PROD=${1}-prod
+Teststelle Weingarten_WRU=${1}-wru
 
-echo "Generating key for $NAME_PROD"
-openssl genrsa -des3 -out $NAME_PROD.key 4096
-echo "Create signing request for $NAME_PROD"
-openssl req -new -key $NAME_PROD.key -out $NAME_PROD.csr -subj "/emailAddress=DL-cao-cwa@t-systems.com/C=DE/ST=Hessen/L=Frankfurt am Main/O=T-Systems International GmbH/OU=schnelltestportal.de/OU=1key/CN="$NAME_PROD".schnelltestportal.de"
+echo "Generating key for $Teststelle Weingarten
+openssl genrsa -des3 -out $Teststelle Weingarten 4096
+echo "Create signing request for $Teststelle Weingarten
+openssl req -new -key $NAME_PROD.key -out $Teststelle Weingarten.csr -subj "/emailAddress=DL-cao-cwa@t-systems.com/C=DE/ST=Hessen/L=Frankfurt am Main/O=T-Systems International GmbH/OU=schnelltestportal.de/OU=1key/CN="$NAME_PROD".schnelltestportal.de"
 
-echo "Generating key for $NAME_WRU"
-openssl genrsa -des3 -out $NAME_WRU.key 4096
-echo "Create signing request for $NAME_WRU"
-openssl req -new -key $NAME_WRU.key -out $NAME_WRU.csr -subj "/emailAddress=DL-cao-cwa@t-systems.com/C=DE/ST=Hessen/L=Frankfurt am Main/O=T-Systems International GmbH/OU=schnelltestportal.de/OU=1key/CN="$NAME_WRU".schnelltestportal.de"
+echo "Generating key for $Teststelle Weingarten"
+openssl genrsa -des3 -out $Teststelle Weingarten 4096
+echo "Create signing request for $Teststelle Weingarten
+openssl req -new -key $NAME_WRU.key -out $Teststelle Weingarten.csr -subj "/emailAddress=DL-cao-cwa@t-systems.com/C=DE/ST=Hessen/L=Frankfurt am Main/O=T-Systems International GmbH/OU=schnelltestportal.de/OU=1key/CN="$NAME_WRU".schnelltestportal.de"
 
 echo -n "Please send following files to cwa-schnelltest-onboarding@t-systems.com: "
 ls *.csr
